86b180bbeebce185ed1b12018701540f6c717cd9
MD.AL-MUKTADIRUL ISLAM IRAM <muktadiruliram681@gmail.com>
2026-06-01 22:24:18 +0600

    Fixed Welcome Message

--- src/UI/DashBoard/Welcome.jsx
+++ src/UI/DashBoard/Welcome.jsx
@@ -5,7 +5,7 @@ const Welcome = ({workSpace,setScreen}) => {
 
   return (
     <div className="flex flex-col justify-center items-center h-[80vh] gap-24">
-      <h1 className="text-2xl md:text-7xl font-semibold">Welcome to <span className="bg-black text-white p-4 rounded-md">{workSpace.workSpaceName}</span></h1>
+      <h1 className="text-2xl md:text-7xl font-semibold">Welcome to <span className="inline-block wrap-break-word max-w-full bg-black text-white p-4 rounded-md">{workSpace.workSpaceName}</span></h1>
       <div className="flex"> 
          <button onClick={()=>setScreen("projects")} className="w-32 h-14 bg-blue-600">Get</button>
       </div>
